798c748083fe59c3090c3bad087f44c4ff31eded
donnasvinis <dvsvinis@mac.com>
2019-06-10 12:28:21 -0400

    Part3 implementing Animal not finished

--- src/main/java/com/zipcodewilmington/assessment2/part3/Horse.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/Horse.java
@@ -1,4 +1,22 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class Horse{
+public class Horse implements Animal{
+    public Horse() {
+
+    }
+
+    @Override
+    public String move() {
+        return "gallup";
+    }
+
+    @Override
+    public int getSpeed() {
+        return 0;
+    }
+
+    @Override
+    public String color() {
+        return null;
+    }
 }
