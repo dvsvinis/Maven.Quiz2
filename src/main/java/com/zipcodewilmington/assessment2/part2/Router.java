e0a27bdb02198bf87cdb867b0f7609ed6402f7df
donnasvinis <dvsvinis@mac.com>
2019-06-11 12:25:33 -0400

    Part 2 all tests except one toString test passing in router.

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,12 +1,13 @@
 package com.zipcodewilmington.assessment2.part2;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Router {
     private String path;
     private String controller;
-    private Map<String, String> routerMap = new HashMap<String, String>();
+    private Map<String, String> routerMap = new LinkedHashMap<String, String>();
 
     public void add(String path, String controller) {
         routerMap.put(path, controller);
@@ -27,4 +28,16 @@ public class Router {
     public void remove(String path) {
         routerMap.remove(path);
     }
+
+
+    public String toString() {
+        String output = "";
+           for (String each : routerMap.keySet()) {
+              output += each;
+              output += getController(each);
+              output += "\n";
+        }
+           return output;
+    }
+
 }
