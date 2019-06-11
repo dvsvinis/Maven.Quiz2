165d0dcd0e1f885b811dc1d31a544666cd31911b
donnasvinis <dvsvinis@mac.com>
2019-06-11 08:52:50 -0400

    Part 2 router addTest passed

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,7 +1,15 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Router {
+    private String path;
+    private String controller;
+    private Map<String, String> routerMap = new HashMap<String, String>();
+
     public void add(String path, String controller) {
+        routerMap.put(path, controller);
     }
 
     public Integer size() {
@@ -9,12 +17,13 @@ public class Router {
     }
 
     public String getController(String path) {
-        return null;
+        return routerMap.get(path);
     }
 
     public void update(String path, String studentController) {
     }
 
     public void remove(String path) {
+
     }
 }
