d10c12aaff4ee6bf8ca726a30c75bfb03ce9f0e9
donnasvinis <dvsvinis@mac.com>
2019-06-11 08:57:58 -0400

    Part 2 router updateController tests passed

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -21,6 +21,7 @@ public class Router {
     }
 
     public void update(String path, String studentController) {
+        routerMap.replace(path, studentController);
     }
 
     public void remove(String path) {
